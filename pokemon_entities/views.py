1846f2b7de4bbdb4ab1fd3c5b5d2333a067ab8c5
Alexander Ten <orionapplepie@gmail.com>
2021-11-20 20:18:57 +1000

    Show page of a specific pokemon.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -58,22 +58,32 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
+    try:
+        requested_pokemon = Pokemon.objects.get(id=pokemon_id)
+    except Pokemon.DoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
+    pokemon_picture_url = request.build_absolute_uri(
+        requested_pokemon.picture.url
+    )
+
+    pokemon = {
+        'title_ru': requested_pokemon.title,
+        'img_url': pokemon_picture_url,
+    }
+
+    pokemon_entities = PokemonEntity.objects.filter(
+        pokemon__id=pokemon_id
+    )
+
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+
+    for pokemon_entity in pokemon_entities:
         add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
+            folium_map,
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            pokemon_picture_url,
         )
 
     return render(request, 'pokemon.html', context={
